b6192022fc068b6923b4c049f2e5ff7b97d3f3ad
manasa-kv <manasa.kv@wissen.com>
2024-07-31 17:11:05 +0530

    Initial commit

---
+++ problemSolving/src/main/java/com/dsa/algo/problemSolving/ProblemSolvingApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.algo.problemSolving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSolvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemSolvingApplication.class, args);
+	}
+
+}
